0a82919c6bef6a52627137862e4537f6b2964753
Leonardo Criado <70546903+LeonardoCriado@users.noreply.github.com>
2025-08-30 19:55:13 -0300

    Migrate project to Godot 4.4 and refactor player
    Updated project files and resources to Godot 4.4 format, including scene, import, and environment files. Refactored Player node to use Sprite2D, added a cannon as a child node, and implemented cannon rotation towards the mouse. Added project metadata files and improved .gitignore handling for Godot 4+.

--- entrega-2-criado-leonardo/entities/player/Player.gd
+++ entrega-2-criado-leonardo/entities/player/Player.gd
@@ -1,18 +1,14 @@
-extends Sprite
+extends Sprite2D
 
 var speed = 200 #Pixeles
+@onready var cannon = $Cañon
 
 func _physics_process(delta):
-	# Manera básica
-	var direction:int = 0
-	if Input.is_action_pressed("move_left"):
-		direction = -1
-	elif Input.is_action_pressed("move_right"):
-		direction = 1
+	# Rotación del cañón hacia el mouse
+	var mouse_position = get_global_mouse_position()
+	var angle = (mouse_position - cannon.global_position).angle()
+	cannon.rotation = angle
 	
-	#position.x += direction * speed * delta
-	
-	# Manera optimizada
-	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
-	position.x += direction_optimized * speed * delta
+	# Movimiento horizontal del jugador
+	var direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	position.x += direction * speed * delta
