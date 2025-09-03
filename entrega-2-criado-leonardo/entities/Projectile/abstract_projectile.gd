7255dff4ed51353ba0eedde007304f4329d9f308
Leonardo Criado <70546903+LeonardoCriado@users.noreply.github.com>
2025-09-03 01:35:08 -0300

    Add turret enemy with firing projectiles
    Introduces a new Turret enemy scene and script that periodically fires projectiles at the player. Updates the main scene to include the turret and connects it to the player and projectile container. Also refactors projectile logic to support turret projectiles and timer-based deletion.

--- entrega-2-criado-leonardo/entities/Projectile/abstract_projectile.gd
+++ entrega-2-criado-leonardo/entities/Projectile/abstract_projectile.gd
@@ -14,10 +14,11 @@ func _ready() -> void:
 func set_starting_values(starting_position:Vector2, direction:Vector2):
 	global_position = starting_position
 	self.direction = direction
+	$Timer.start()
 	set_physics_process(true)
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 	
 func _on_timer_timeout():
-	emit_signal("delete_requested")
+	emit_signal("delete_requested", self)
