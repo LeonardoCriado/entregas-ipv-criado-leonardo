72dd1ee33a8e278410827acecf190e8e48e46599
Leonardo Criado <70546903+LeonardoCriado@users.noreply.github.com>
2025-09-10 21:38:03 -0300

    Enable turret to detect and fire at player
    Added Area2D and collision shape to Turret for player detection. Turret now starts firing when the player enters its area. Player class is now named for type checking, and collision layers are updated for proper interaction.

--- entrega-3-criado-leonardo/entities/enemies/Turret.gd
+++ entrega-3-criado-leonardo/entities/enemies/Turret.gd
@@ -2,17 +2,18 @@ extends Sprite2D
 
 @onready var fire_position: Node2D = $FirePosition
 @onready var fire_timer: Timer = $FireTimer
-
 @export var projectile_scene: PackedScene
 
 var projectile_container: Node
 var targuet:Node2D
 
+func _ready() -> void:
+	fire_timer.timeout.connect(fire_at_player)
+
 func initialize(turret_pos: Vector2, projectile_container: Node) -> void:
 	global_position = turret_pos
 	self.projectile_container = projectile_container
-#	fire_timer.connect("timeout", fire_at_player)
-#	fire_timer.start()
+
 
 func fire_at_player() -> void:
 	var proj_instance = projectile_scene.instantiate()
@@ -21,3 +22,12 @@ func fire_at_player() -> void:
 		fire_position.global_position,
 		fire_position.global_position.direction_to(targuet.global_position)
 	)
+	print(fire_position.position.direction_to(targuet.position))
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Player")  
+	print(body.global_position)
+	if body is Player:
+		targuet = body
+		fire_timer.start()
