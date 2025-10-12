a7d5ef219256c72a92fcfe6b6fde365df58e2be4
Leonardo Criado <70546903+LeonardoCriado@users.noreply.github.com>
2025-10-12 18:58:29 -0300

    Add options and return buttons to PauseMenu
    PauseMenu now includes an Options button to open the OptionsMenu and a 'Back to Menu' button that emits a signal to return to the main menu. Related UI layout adjustments and signal connections were added to support these features.

--- entrega-6-criado-leonardo/src/game/ui/PauseMenu.gd
+++ entrega-6-criado-leonardo/src/game/ui/PauseMenu.gd
@@ -3,13 +3,18 @@ extends Control
 ## Menú de pausa genérico, abierto utilizando la acción "pause_menu"
 ## (por default la tecla Esc).
 
+# Regresa al menu principal
+signal return_requested()
+
+@onready var options_menu: Control = $OptionsMenu
+
 
 func _ready() -> void:
 	hide()
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("pause_menu"):
+	if event.is_action_released("pause_menu") && !options_menu.visible:
 		visible = !visible
 		get_tree().paused = visible
 
@@ -17,3 +22,9 @@ func _unhandled_input(event: InputEvent) -> void:
 func _on_resume_button_pressed() -> void:
 	hide()
 	get_tree().paused = false
+	
+
+
+
+func _on_rerturn_button_pressed() -> void:
+	return_requested.emit()
